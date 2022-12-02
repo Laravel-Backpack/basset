3cf927b8b30cc8c0dadd38406352c8952cca0dfb
Antonio Almeida <promatik@gmail.com>
2022-12-02 00:42:31 +0000

    Added bassets feature

--- src/AssetManager.php
+++ src/AssetManager.php
@@ -2,27 +2,47 @@
 
 namespace DigitallyHappy\Assets;
 
+use Exception;
+
 class AssetManager
 {
-    public $loaded;
+    const STATUS_DISABLED = 'Cache CDN is disabled in the configuration.';
+    const STATUS_LOCAL = 'Asset is not in a CDN.';
+    const STATUS_IN_CACHE = 'Asset was already in cache.';
+    const STATUS_DOWNLOADED = 'Asset downloaded.';
+    const STATUS_NO_ACTION = 'Asset was not downloaded, falling back to CDN.';
+
+    private $loaded;
 
     public function __construct()
     {
         $this->loaded = [];
     }
 
-    public function echoCss($path)
+    /**
+     * Outputs a file depending on its type
+     *
+     * @param string $path
+     * @return void
+     */
+    public function echoFile(string $path, array $attributes = []): void
     {
-        if ($this->isLoaded($path)) {
-            return;
+        if (substr($path, -3) === '.js') {
+            $this->echoJs($path, $attributes);
         }
 
-        $this->markAsLoaded($path);
-
-        echo '<link href="'.asset($path).'" rel="stylesheet" type="text/css" />';
+        if (substr($path, -4) === '.css') {
+            $this->echoCss($path, $attributes);
+        }
     }
 
-    public function echoJs($path)
+    /**
+     * Outputs the CSS link tag
+     *
+     * @param string $path
+     * @return void
+     */
+    public function echoCss(string $path, array $attributes = []): void
     {
         if ($this->isLoaded($path)) {
             return;
@@ -30,7 +50,34 @@ class AssetManager
 
         $this->markAsLoaded($path);
 
-        echo '<script src="'.asset($path).'"></script>';
+        $args = '';
+        foreach ($attributes as $key => $value) {
+            $args .= " $key".($value === true || empty($value) ? '' : "=\"$value\"");
+        }
+
+        echo '<link href="'.asset($path).'"'.$args.' rel="stylesheet" type="text/css" />';
+    }
+
+    /**
+     * Outputs the JS script tag
+     *
+     * @param string $path
+     * @return void
+     */
+    public function echoJs(string $path, array $attributes = []): void
+    {
+        if ($this->isLoaded($path)) {
+            return;
+        }
+
+        $this->markAsLoaded($path);
+
+        $args = '';
+        foreach ($attributes as $key => $value) {
+            $args .= " $key".($value === true || empty($value) ? '' : "=\"$value\"");
+        }
+
+        echo '<script src="'.asset($path).'"'.$args.'></script>';
     }
 
     /**
@@ -39,7 +86,7 @@ class AssetManager
      * @param  string  $asset
      * @return void
      */
-    public function markAsLoaded($asset)
+    public function markAsLoaded(string $asset): void
     {
         if (! $this->isLoaded($asset)) {
             $this->loaded[] = $asset;
@@ -52,13 +99,9 @@ class AssetManager
      * @param  string  $asset
      * @return bool
      */
-    public function isLoaded($asset)
+    public function isLoaded(string $asset): bool
     {
-        if (in_array($asset, $this->loaded)) {
-            return true;
-        }
-
-        return false;
+        return in_array($asset, $this->loaded);
     }
 
     /**
@@ -66,8 +109,69 @@ class AssetManager
      *
      * @return array
      */
-    public function loaded()
+    public function loaded(): array
     {
         return $this->loaded;
+    }
+
+    /**
+     * Localize a CDN asset
+     *
+     * @param string $asset
+     * @return void
+     */
+    public function basset(string $asset, bool $output = true, array $attributes = []): string
+    {
+        // Valiate user configuration
+        if (! config('digitallyhappy.assets.cache_cdns')) {
+            return self::STATUS_DISABLED;
+        }
+
+        // Make sure asset() is removed
+        $asset = str_replace(asset(''), '', $asset);
+
+        // Validate the asset comes from a CDN
+        if (substr($asset, 0, 4) !== 'http') {
+            $output && $this->echoFile($asset, $attributes);
+            return self::STATUS_LOCAL;
+        }
+
+        $localizedFilePath = config('digitallyhappy.assets.cache_directory').'/'.str_replace(['http://', 'https://', '://'], '', strrpos($asset, '?') ? substr($asset, 0, strrpos($asset, '?')) : $asset);
+        $localizedPath = substr($localizedFilePath, 0, strrpos($localizedFilePath, '/'));
+
+        // Check if asset exists in public/bassets/...
+        if (file_exists($localizedFilePath)) {
+            $output && $this->echoFile($localizedFilePath, $attributes);
+            return self::STATUS_IN_CACHE;
+        }
+
+        // Create the directory
+        if (! is_dir($localizedPath)) {
+            mkdir($localizedPath, recursive:true);
+        }
+
+        // Download the file
+        try {
+            $curl = curl_init();
+            curl_setopt_array($curl, [
+                CURLOPT_FILE => fopen($localizedFilePath, 'w'),
+                CURLOPT_TIMEOUT => 5, // seconds
+                CURLOPT_URL => $asset,
+            ]);
+            curl_exec($curl);
+            $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            curl_close($curl);
+        } catch (Exception $e) {
+            $httpCode = 500;
+        }
+
+        if ($httpCode === 200) {
+            $output && $this->echoFile($localizedFilePath, $attributes);
+            return self::STATUS_DOWNLOADED;
+        }
+
+        // Fallback to the CDN
+        $output && $this->echoFile($asset, $attributes);
+        return self::STATUS_NO_ACTION;
     }
 }
