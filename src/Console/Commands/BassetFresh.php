d18ac9e022f134dc04b47d4e2a92486f84135c93
Antonio Almeida <promatik@gmail.com>
2023-06-19 00:19:14 +0100

    Added basset fresh command

---
+++ src/Console/Commands/BassetFresh.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Backpack\Basset\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+/**
+ * Basset Fresh command.
+ *
+ * @property object $output
+ */
+class BassetFresh extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'basset:fresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear and Cache the basset directory';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $this->call('basset:clear');
+        $this->call('basset:cache');
+    }
+}
