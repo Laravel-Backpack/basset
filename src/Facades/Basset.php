f7f71b1c5774c372a53b12863cd6702a5dbebefe
Antonio Almeida <promatik@gmail.com>
2023-06-22 00:42:53 +0100

    Revert "Fixed @mixin"
    This reverts commit 14ac66e88f70b6cdd01c2412647e05c9c78d9eb2.

--- src/Facades/Basset.php
+++ src/Facades/Basset.php
@@ -8,6 +8,20 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class Basset Facade.
  *
+ * @method static void markAsLoaded(string $asset)
+ * @method static bool isLoaded(string $asset)
+ * @method static array loaded()
+ * @method static void echoFile(string $path, array $attributes = [])
+ * @method static void echoCss(string $path, array $attributes = [])
+ * @method static void echoJs(string $path, array $attributes = [])
+ * @method static string getPath(string $asset)
+ * @method static string getPathHashed(string $asset, string $content)
+ * @method static string getUrl(string $asset)
+ * @method static StatusEnum basset(string $asset, bool|string $output = true, array $attributes = [])
+ * @method static StatusEnum bassetBlock(string $asset, string $code, bool $output = true)
+ * @method static StatusEnum bassetArchive(string $asset, string $output)
+ * @method static StatusEnum bassetDirectory(string $asset, string $output)
+ *
  * @mixin BassetManager
  */
 class Basset extends Facade
