207a312c27594361bab23dcd16b2b9a1349aa0da
Pedro X. <pxpm88@gmail.com>
2024-04-08 14:07:35 +0100

    store the file with the provided extension

--- src/BassetManager.php
+++ src/BassetManager.php
@@ -21,13 +21,19 @@ class BassetManager
     use Traits\ViewPathsTrait;
 
     private FilesystemAdapter $disk;
+
     private array $loaded;
+
     private string $basePath;
+
     private bool $dev = false;
 
     public CacheMap $cacheMap;
+
     public LoadingTime $loader;
+
     public Unarchiver $unarchiver;
+
     public FileOutput $output;
 
     public function __construct()
@@ -90,12 +96,13 @@ class BassetManager
      * @param  string  $asset
      * @return string
      */
-    public function getPath(string $asset): string
+    public function getPath(string $asset, string $extension = null): string
     {
         return Str::of($this->basePath)
             ->append(str_replace([base_path().'/', base_path(), 'http://', 'https://', '://', '<', '>', ':', '"', '|', "\0", '*', '`', ';', "'", '+'], '', $asset))
             ->before('?')
-            ->replace('/\\', '/');
+            ->replace('/\\', '/')
+            ->finish($extension ?? '');
     }
 
     /**
@@ -138,8 +145,10 @@ class BassetManager
     {
         $this->loader->start();
 
+        $extension = empty($extension) ? $extension : Str::start($extension, '.');
+
         // Get asset path
-        $path = $this->getPath(is_string($output) ? $output : $asset);
+        $path = $this->getPath(is_string($output) ? $output : $asset, $extension);
 
         if ($this->isLoaded($path)) {
             return $this->loader->finish(StatusEnum::LOADED);
