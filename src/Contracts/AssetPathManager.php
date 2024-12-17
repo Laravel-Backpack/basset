0d23dbb585520419f3e756ddff1c22d3ee555cc9
tabacitu <tabacitu@users.noreply.github.com>
2024-12-17 10:42:08 +0000

    Apply fixes from StyleCI

--- src/Contracts/AssetPathManager.php
+++ src/Contracts/AssetPathManager.php
@@ -5,6 +5,8 @@ namespace Backpack\Basset\Contracts;
 interface AssetPathManager
 {
     public function getBasePath(): string;
+
     public function isLocal(string $path): bool;
+
     public function getPathOnDisk(string $path): string;
 }
